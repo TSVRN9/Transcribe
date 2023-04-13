07e91e65b898006b15491557e5455feeec6353ce
tavern <66744142+TSVRN9@users.noreply.github.com>
2023-04-13 10:29:37 -0400

    Setup SvelteKitPWA, Tailwind, and gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,43 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			base: '/Transcribe',
+			manifest: {
+				short_name: 'Transcribe',
+				name: 'Transcribe',
+				display: 'standalone',
+				theme_color: "#000000",
+				background_color: "#000000",
+//				icons: [
+//					{
+//						src: '/pwa-192x192.png',
+//						sizes: '192x192',
+//						type: 'image/png',
+//					},
+//					{
+//						src: '/pwa-512x512.png',
+//						sizes: '512x512',
+//						type: 'image/png',
+//					},
+//					{
+//						src: '/pwa-512x512.png',
+//						sizes: '512x512',
+//						type: 'image/png',
+//						purpose: 'any maskable',
+//					},
+//				],
+			},
+			injectManifest: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+		})
+	]
 });
