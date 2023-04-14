ecd430e4ad350e123a31ac2569f114d998c52fdc
tavern <66744142+TSVRN9@users.noreply.github.com>
2023-04-14 10:47:25 -0400

    Allow picking and playing an audio file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
-<!-- Testing Tailwind!! -->
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let files: FileList | undefined;
+    $: audioFile = files == undefined ? undefined : files[0];
+    $: console.log(audioFile); // ** DEBUG
+</script>
+
+<h1>Transcribe!</h1>
+<input type="file" accept=".mp3, .ogg, .wav" bind:files>
+{#if audioFile}
+    <audio controls src={URL.createObjectURL(audioFile)} />
+{/if}
